1a15c975670ccab741705e1baa7eaebd5c6c597b
reisma <reisma@reisma-m11x>
2012-10-26 21:38:41 +0200

    A First example to a simple java gamex

--- src/javagame/JavaGame.java
+++ src/javagame/JavaGame.java
@@ -11,8 +11,16 @@ package javagame;
  * @author reisma
  * @version 0.1, 25.10.2012
  */
-public class JavaGame {
+public class JavaGame{
 
+    /**
+     * Constructor of the JavaGame Class
+     * It simple gives a Hello World output on the console
+     */
+    public JavaGame(){
+          
+        System.out.println("Hello World!");
+    }
     /**
      * Main Method which is the start point for you Programm
      * This means a call on the command line like:"C:\> java JavaGame"
@@ -23,26 +31,8 @@ public class JavaGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /** Use the System class's out.println Method to Show a text
-         * <p>
-         * <em>System</em>: provides standard input, standard output, and error output 
-         * streams;
-         * access to externally defined properties and environment variables;
-         * a means of loading files and libraries;
-         * and a utility method for quickly copying a portion of an array.
-         * <p>
-         * <em>out</em>: The "standard" output stream.
-         * This stream is already open and ready to accept output data. 
-         * Typically this stream corresponds to display output or another output
-         * destination specified by the host environment or user.
-         * <p>
-         * <em>println</em>: Prints an object. 
-         * The string produced by the String.valueOf(Object) method is 
-         * translated into bytes according to the platform's default character 
-         * encoding, and these bytes are written in exactly the manner of the 
-         * PrintStream.write(int) method.
-         */                
-        System.out.println("Hello World!");
-               
+        // create an Object of the JavaGame class
+        new JavaGame();
+        
     }
 }
